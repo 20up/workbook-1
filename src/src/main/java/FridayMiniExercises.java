58e67e57ad012177b71f18902f0608e9732b6005
Panom <panompreston@gmail.com>
2025-04-11 17:49:42 -0700

    add friday mini exercises

---
+++ src/src/main/java/FridayMiniExercises.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class FridayMiniExercises {
+    public static void main(String[] args) {
+        //make scanner
+        Scanner scanner = new Scanner(System.in);
+
+//        //1 Say Hello
+//        System.out.print("what is your name:"); //prints the question
+//        String Name = scanner.next();//lets you write the answer
+//        System.out.println("Hello, " + Name + " welcome to the game.");//prints ur answer in the sentence
+//
+//        //2 Age in Dog Years
+//        System.out.print("what is your age:");
+//        int age = scanner.nextInt();
+//        int dogY = age * 7;
+//        System.out.println("age in dog years:" + dogY);
+//
+//        //3 Dice Roll
+//        double random = Math.random();
+//        double diceNrb = Math.round(random * 6);
+//        System.out.println("Random dice number:" + diceNrb);
+//
+//        //4 hero state
+//        //hero name
+//        System.out.print("hero name:");
+//        String heroN = scanner.nextLine();
+//        //hero strengths
+//        System.out.print("hero's strength:");
+//        int heroS = scanner.nextInt();
+//        //agility
+//        System.out.print("hero agility:");
+//        int heroA = scanner.nextInt();
+//        //Hero Stats
+//        System.out.println("Name:" + heroN + " Strength:" + heroS + " Agility:" + heroA);
+//
+//        //5 Level up
+//        //current level
+//        System.out.print("Current Level:");
+//        int current_level = scanner.nextInt();
+//        //level up
+//        int level_up = current_level + 1;
+//        System.out.println("You Leveled up! You are now level " + level_up);
+//
+//        //6 coin calculator
+//        //silver coins
+//        System.out.print("how much silver do you have:");
+//        int silver = scanner.nextInt();
+//        int silver_calc = silver * 1;
+//        //gold coins
+//        System.out.print("how much gold do you have:");
+//        int gold = scanner.nextInt();
+//        int gold_calc = gold * 10;
+//        //calc how much you have
+//        int coin_total = gold_calc + silver_calc;
+//        System.out.println("your total amount of coins is: " + coin_total);
+//
+//        //7 Odd or even dice
+//        double random = Math.random();
+//        double Dice_number = Math.round(random * 6);
+//        System.out.println("Random dice number:" + Dice_number);
+//        if (Dice_number % 2 == 0) {
+//            System.out.println(" -number is even- ");
+//        } else {
+//            System.out.println(" -number is odd- ");
+//        }
+          //8 critical hit chance
+
+
+
+    }
+}
+
